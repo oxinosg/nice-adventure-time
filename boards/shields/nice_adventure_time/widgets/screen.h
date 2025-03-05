da3515902405fbc1258c90951c5d8b3cfc4d58ad
whoop-t <tommymallis@gmail.com>
2025-03-05 09:05:35 -0800

    shouldnt do anything

--- boards/shields/nice_adventure_time/widgets/screen.h
+++ boards/shields/nice_adventure_time/widgets/screen.h
@@ -8,7 +8,6 @@ struct zmk_widget_screen {
     sys_snode_t node;
     lv_obj_t *obj;
     lv_color_t cbuf[BUFFER_SIZE * BUFFER_SIZE];
-    lv_color_t cbuf2[BUFFER_SIZE * BUFFER_SIZE];
     lv_color_t cbuf3[BUFFER_SIZE * BUFFER_SIZE];
     struct status_state state;
 };
