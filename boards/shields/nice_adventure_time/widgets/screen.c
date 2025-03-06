5650680faa10345f4a2500d7825b1f3f50dfbae4
whoop-t <tommymallis@gmail.com>
2025-03-06 11:25:13 -0800

    finn and jake battery states may be causing problems

--- boards/shields/nice_adventure_time/widgets/screen.c
+++ boards/shields/nice_adventure_time/widgets/screen.c
@@ -62,7 +62,6 @@ static void set_battery_status(struct zmk_widget_screen *widget,
     widget->state.battery = state.level;
 
     draw_top(widget->obj, widget->cbuf, &widget->state);
-    draw_finn(widget->obj, &widget->state);
 }
 
 static void battery_status_update_cb(struct battery_status_state state) {
@@ -144,6 +143,8 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_obj_t *bottom = lv_canvas_create(widget->obj);
     lv_obj_align(bottom, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_BOTTOM, 0);
     lv_canvas_set_buffer(bottom, widget->cbuf3, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
+  
+    draw_finn(widget->obj);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
